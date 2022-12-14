13632e3546efceb33437496f83dad902169803ab
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2022-12-14 11:47:35 +0530

    updated tf gpu test code

--- notes/TF/gpu_test.py
+++ notes/TF/gpu_test.py
@@ -1,2 +1,8 @@
 import tensorflow as tf
+print(tf.__version__)
 print(tf.test.gpu_device_name())
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(len(gpus))
+for gpu in gpus:
+    print("Name:", gpu.name, "  Type:", gpu.device_type)
